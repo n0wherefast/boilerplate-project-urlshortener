b4bca1b3fe457028f50d2ecbb58060a495c9f95c
n0wherefast <97000096+n0wherefast@users.noreply.github.com>
2024-06-27 01:12:55 +0200

    fcc urlshortner solution

--- index.js
+++ index.js
@@ -2,11 +2,23 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const {MongoClient} = require('mongodb')
+
+const uriPROV ='mongodb+srv://FCCtraining:fcctrain1@cluster0.qquxbky.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+
+const client = new MongoClient(uriPROV)
+const db = client.db("urlshortner")
+const urls = db.collection("urls")
+const dns = require('dns')
+const urlparser =require('url');
+const { error } = require('console');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -15,9 +27,31 @@ app.get('/', function(req, res) {
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post('/api/shorturl', function(req, res) {
+  console.log(req.body)
+  const url = req.body.url
+  const dnslookup = dns.lookup(urlparser.parse(url).hostname,async(err,address) =>{
+    if(!address){
+      res.json({error:'URL not Valid'})
+    }
+    else{
+      const urlCount = await urls.countDocuments({})
+      const urlDoc ={
+        url,
+        short_url: urlCount
+      }
+       const result = await urls.insertOne(urlDoc)
+       console.log(result);
+       res.json({original_url: url,short_url:urlCount})
+    }
+  })
 });
+
+app.get("/app/short_url/:short_url",async (req,res)=>{
+  const shorturl = req.params.short_url
+  const urlDoc = await urls.findOne({short_url: +shorturl})
+  res.redirect(urlDoc.url)
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
